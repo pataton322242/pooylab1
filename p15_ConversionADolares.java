485ca8cf21e00db53dea42d4fde1a77f7c87ef01
pataton322242 <colmeneromartin318@gmail.com>
2024-08-19 08:33:18 -0600

    practica1.1

--- p15_ConversionADolares.java
+++ p15_ConversionADolares.java
@@ -4,18 +4,11 @@ public class p15_ConversionADolares {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Pedimos la cantidad en pesos
         System.out.print("Ingresa la cantidad en pesos: ");
         double cantidadPesos = scanner.nextDouble();
-        
-        // Pedimos la cotización del dólar
         System.out.print("Ingresa la cotización del dólar: ");
         double cotizacionDolar = scanner.nextDouble();
-        
-        // Calculamos el equivalente en dólares
         double cantidadDolares = cantidadPesos / cotizacionDolar;
-        
-        // Mostramos el resultado
         System.out.println("El equivalente en dólares es: " + cantidadDolares);
         
         

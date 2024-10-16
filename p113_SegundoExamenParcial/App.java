bedabfddc161020e192742c7da2edc6527ba5351
pataton322242 <colmeneromartin318@gmail.com>
2024-10-16 08:23:04 -0600

    T

--- p113_SegundoExamenParcial/App.java
+++ p113_SegundoExamenParcial/App.java
@@ -10,14 +10,14 @@ public class App {
         miequipo.reporte();
 
         
-        miequipo.agregarJugador(new JugadorEntrenador("Francisco Zapeda", 'H', "Entrenador Principal", 1000, 8, 1));
-        miequipo.agregarJugador(new JugadorEntrenador("Alexis Estrada", 'H', "Entrenador de Apoyo", 900, 10, 2));
-        miequipo.agregarJugador(new JugadorActivo("Alexander Lopez", 'H', "Principal No 11", 100, 2, 2));
-        miequipo.agregarJugador(new JugadorActivo("Uriel Puga", 'H', "Principal No 16", 100, 3, 1));
-        miequipo.agregarJugador(new JugadorActivo("Alejandra Escalona", 'M', "Principal No 21", 100, 3, 3));
-        miequipo.agregarJugador(new JugadorActivo("Armando Santana", 'H', "Banca No 10", 120, 4, 1));
-        miequipo.agregarJugador(new JugadorActivo("Daniel Mijares", 'H', "Banca No 15", 150, 4, 4));
-        miequipo.agregarJugador(new JugadorActivo("Antonio Hernandez", 'M', "Banca No 20", 350, 5, 3));
+        miequipo.agregarJugador(new JugadorEntrenador("MAX MONTOYA", 'H', "Entrenador Principal", 1000, 8, 1));
+        miequipo.agregarJugador(new JugadorEntrenador("LUIS OBED", 'H', "Entrenador de Apoyo", 900, 10, 2));
+        miequipo.agregarJugador(new JugadorActivo("MARTIN COLMENRO", 'H', "Principal No 11", 100, 2, 2));
+        miequipo.agregarJugador(new JugadorActivo("AARON REYES", 'H', "Principal No 16", 100, 3, 1));
+        miequipo.agregarJugador(new JugadorActivo("LESLEY MARQUEZ", 'M', "Principal No 21", 100, 3, 3));
+        miequipo.agregarJugador(new JugadorActivo("MARCO ANTONIO OLIVA", 'H', "Banca No 10", 120, 4, 1));
+        miequipo.agregarJugador(new JugadorActivo("VICTOR CERVANTES", 'H', "Banca No 15", 150, 4, 4));
+        miequipo.agregarJugador(new JugadorActivo("VIVIANA MEDINA", 'M', "Banca No 20", 350, 5, 3));
         miequipo.agregarJugador(new JugadorActivo("Andrea Solis", 'M', "Principal No 12", 550, 5, 5));
         miequipo.agregarJugador(new JugadorActivo("Marissa Hernandez", 'M', "Principal No 12", 600, 4, 2));
         miequipo.agregarJugador(new JugadorActivo("Diana Soto", 'M', "Principal No 12", 700, 5, 3));
